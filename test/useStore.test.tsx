7bc51fecba525afbcf6ab77e1eff177dfb708cc5
João Pedro M. Trabbold <jpmtrabbold@gmail.com>
2020-11-10 11:39:06 +1300

    pub

--- test/useStore.test.tsx
+++ test/useStore.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Example } from '../stories/useStoreLocalWithPropsAsObservableSource.stories';
 
-describe('Thing', () => {
+describe('Example', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Example />, div);
+    ReactDOM.render(<Example sentence='test test'/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
